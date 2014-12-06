facf14d1a3d9ee5d04cf66adde294f18594249aa
TomitaMasakuni <TomitaMasakuni@next-group.jp>
2014-12-05 23:30:40 -0500

    修正　

--- services/gnavi/api.rb
+++ services/gnavi/api.rb
@@ -7,7 +7,7 @@ module Gnavi
 
     HOST = 'http://api.gnavi.co.jp'
     PATH_SEARCH = '/ver1/RestSearchAPI/'
-    KEY_ID = '4b4b7cb1e0439e9ce9069e0ab1b4f947'
+    KEY_ID = ''
 
     def self.search(params = {})
       freeword = params[:freeword]
